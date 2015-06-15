9c1ddcc1539227fb698bb3b7a165dad331196282
Sedatcan Sonat <sedatcans@Sedatcan-Mini.mynet>
2015-06-15 22:07:04 +0300

    Initial Commit

---
+++ Application Practices/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Application Practices
+//
+//  Created by Sedatcan Sonat on 15.06.2015.
+//  Copyright (c) 2015 Sedatcan Sonat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
